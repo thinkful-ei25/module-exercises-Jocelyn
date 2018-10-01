f2a91ee595535d9f5e935d293568476e75fac747
jsantiag <jsantiag@wellesley.edu>
2018-10-01 11:27:35 -0500

    updated methods in shoppingList

--- scripts/store.js
+++ scripts/store.js
@@ -10,11 +10,47 @@ const store = (function(){
 
   const hideCheckedItems = false;
   const searchTerm = '';
+
+  const findbyId = function(id) {
+    store.items.find(item => item.id === id);
+  };
+
+  const addItem = function(name){
+    try{
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(eroor) {
+      console.log('Cannot add item: '+ error.message)
+    }
+  };
+  const findAndToggleChecked = function(id) {
+    let found = this.findbyId(id);
+    found.checked = !found.checked;
+  };
+
+  const findAndUpdateName = function(id, newName){
+    try{
+      Item.validateName(newName);
+      let found = findbyId(id); 
+      found.name = newName;
+    }catch(error){
+      console.log('cannot update name: '+ error.message)
+    }
+  };
+
+  const findAndDelete = function(id){
+    delete this.items.filter(id);
+  };
+
   return {
     items, 
     hideCheckedItems,
     searchTerm,
-    
+    findbyId,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
   };
 
 }());
